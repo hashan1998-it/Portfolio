fabdeb037c9a3764508f7297b0368d4ab40302b6
Hashan Danushka <hashansooriyage@gmail.com>
2023-12-27 12:45:38 +0530

    Initial Commit

---
+++ src/utils/Themes.js
@@ -0,0 +1,32 @@
+export const darkTheme = {
+    bg:"#1C1C27",
+    bgLight: "#1C1E27",
+    primary:"#854CE6",
+    text_primary:"#F2F3F4",
+    text_secondary:"#b1b2b3",
+    card:"#171721",
+    card_light: '#191924',
+    button:"#854CE6",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    // bg:"#FFFFFF",
+    // bgLight: "#f0f0f0",
+    // primary:"#be1adb",
+    // text_primary:"#111111",
+    // text_secondary:"#48494a",
+    // card:"#FFFFFF",
+    // button:"#5c5b5b",
+    bg: "#EAF6FF",         // Light blue background
+    bgLight: "#D6EAF8",     // Lighter blue background
+    primary: "#3498DB",     // Blue primary color
+    text_primary: "#2C3E50", // Dark text color
+    text_secondary: "#7F8C8D", // Grayish text color
+    card: "#ECF0F1",        // Light gray card background
+    card_light: '#BDC3C7',   // Lighter gray card background
+    button: "#3498DB",       // Blue button color
+    white: "#FFFFFF",
+    black: "#000000",
+}
